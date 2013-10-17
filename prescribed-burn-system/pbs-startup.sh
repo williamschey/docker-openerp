55d8b31b815125b7e94e758a63b0d30d26bcc21c
Tomas Krajca <tomas.krajca@dpaw.wa.gov.au>
2013-10-17 15:03:45 +0800

    Playing with stuff and pbs (no new images pushed to docker registry).

---
+++ prescribed-burn-system/pbs-startup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+
+function provision()
+{
+    $start_db
+    echo "Provisioning ...";
+}
+
+function run()
+{
+    $apache2_start
+    echo "Running ...";
+}
+
+SCRIPT="`basename $0`"
+if [ "$SCRIPT" == "provision" ]; then
+    TASK="Provision"
+elif [ "$SCRIPT" == "run" ]; then
+    TASK="Run"
+elif [ "$SCRIPT" == "provisionAndRun" ]; then
+    TASK="ProvisionAndRun"
+else
+    TASK=${1:-Run}
+fi
+
+
+if [ "${TASK^^}" == "PROVISION" ]; then
+    provision
+elif [ "${TASK^^}" == "RUN" ]; then
+    run
+elif [ "${TASK^^}" == "PROVISIONANDRUN" ]; then
+    provision
+    run
+else
+    echo "Usage: $0 [Run|Provision|ProvisionAndRun]" >&2
+    exit 1
+fi
