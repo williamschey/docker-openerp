5fe3e5642bdee66e058972f49aff52e1ac1793b6
Florian Mayer <Florian.Mayer@dpaw.wa.gov.au>
2013-11-20 13:59:05 +0800

    initialised ALA image

--- ala/README.md
+++ ala/README.md
@@ -1,5 +1,7 @@
 # Overview
 A docker image to run the Atlas of Living Australia.
+This build automates the setup steps of the 
+[ALA Google Code Wiki](http://code.google.com/p/alageospatialportal/wiki/GettingStarted).
 
 Author Department of Parks and Wildlife <florian.mayer@dpaw.wa.gov.au>
 Licence Apache v2
@@ -9,3 +11,4 @@ TODO
 
 # Configuration
 TODO
+
--- ala/startup.sh
+++ ala/startup.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
 #sed -i "s/.*NO_START=.*/NO_START=0/g" /etc/default/jetty
-#sed -i "s/.*JETTY_PORT=.*/JETTY_PORT=$solr_port/g" /etc/default/jetty
-#sed -i "s/#solr_url/solr_url/g" $ckan_etc/paster.ini
 #sed -i "s/:\d+\/solr/:$solr_port\/solr/g" $ckan_etc/paster.ini
-#. $ckan_venv/bin/activate
-#cd $ckan_venv/src/ckan
-#$db_start
-#$solr_start
+
 if [ "$1" == "init" ]; then
-    #paster db init -c $ckan_etc/paster.ini
     echo 'Running configuration steps...'
+    # config steps
+    echo 'Configuration steps finished.'
+    
 else 
-    #paster serve $ckan_etc/paster.ini
     echo 'Starting the server...'
-    #rstudio-server restart
+    # start stuff
 fi
