3eb04920540604ab5348525b51814307f99b5c42
Adon Metcalfe <adon.metcalfe@gmail.com>
2014-01-07 13:10:24 +0800

    simpler build

--- ubuntu/build.sh
+++ ubuntu/build.sh
@@ -3,9 +3,4 @@
 # in other DockerFile's FROM statements
 NAME=docker.dpaw.wa.gov.au/ubuntu
 TAG=12.04
-set -e
-if [ "$1" == "flatten" ]; then
-    docker export $(docker run -d $NAME bash) | pv | docker import - $NAME:$TAG
-else
-    docker build $@ -t $NAME . 
-fi
+docker build $@ -t $NAME:$TAG . 
